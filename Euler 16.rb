b61c5dad4c689d86aa85d5518a5efc21f6570847
Jonathan Davis <jondavis@alumni.unc.edu>
2014-02-21 00:01:46 -0500

    Deleting duplicate files created by the file rename

--- Euler 16.rb
+++
@@ -1,9 +0,0 @@
-=begin
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-
-What is the sum of the digits of the number 21000?
-=end
-
-power = (2**1000).to_s.split(//)
-power.collect! {|x| x.to_i}
-puts power.inject(:+)
